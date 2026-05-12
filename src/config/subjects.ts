66e703ac27b906c9097a3e46066077edab3d620e
maximenewman <mnn6@sfu.ca>
2026-05-11 17:09:20 -0700

    chore: added subjects

--- src/config/subjects.ts
+++ src/config/subjects.ts
@@ -22,4 +22,32 @@ export const subjects: Subject[] = [
   //     { type: 'site', url: 'https://coursys.sfu.ca/2026sp-cmpt-307-d1/' },
   //   ],
   // },
+  {
+    id: 'cmpt307',
+    name: 'CMPT 307',
+    professor: 'TBD',
+    destinationFolder: 'D:/Desktop/University/Summer 2026/CMPT 307',
+    sources: [
+      { type: 'email', label: 'CMPT 307' },
+      { type: 'site', url: 'https://coursys.sfu.ca/2026su-cmpt-307-d1/pages/' },
+    ],
+  },
+  {
+    id: 'stat271',
+    name: 'STAT 271',
+    professor: 'TBD',
+    destinationFolder: 'D:/Desktop/University/Summer 2026/STAT 271',
+    sources: [
+      { type: 'email', label: 'STAT 271' },
+    ],
+  },
+  {
+    id: 'stat302',
+    name: 'STAT 302',
+    professor: 'TBD',
+    destinationFolder: 'D:/Desktop/University/Summer 2026/STAT 302',
+    sources: [
+      { type: 'email', label: 'STAT 302' },
+    ],
+  },
 ];
